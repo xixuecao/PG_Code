c58f7a32b475cd2fd1453c0e4e8c21bc64caee12
xixuecao <xixuecao@gmail.com>
2019-07-28 09:17:36 +0800

    section 7.1.1

---
+++ C++/7_1_1/7_1_1/7_1_1.cpp
@@ -0,0 +1,47 @@
+﻿// 7_1_1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "Sales_item.h"
+using namespace std;
+
+
+int main()
+{
+	cout << "Input your record(ISBN, units_sold, revenue, price): " << endl;
+	Sales_item total;
+	if (cin >> total)
+	{
+		Sales_item trans;
+		while (cin >> trans)
+		{
+			if (total.isbn() == trans.isbn())
+			{
+				total += trans;
+			}
+			else
+			{
+				cout << total << endl;
+				total = trans;
+			}
+		}
+		cout << total << endl;
+	}
+	else
+	{
+		cerr << "No Data ?" << endl;
+		return -1;
+	}
+	return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
