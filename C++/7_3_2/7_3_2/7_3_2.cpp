1d8ff6227d0deeafb120c8f14c49c1a50c69c0a9
xixuecao <xixuecao@gmail.com>
2019-08-03 08:00:23 +0800

    section 7.3.2

---
+++ C++/7_3_2/7_3_2/7_3_2.cpp
@@ -0,0 +1,36 @@
+﻿// 7_3_2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "Screen.h"
+using namespace std;
+
+
+int main()
+{
+	/*
+	Screen myScreen(5, 3);
+	const Screen blank(5, 3, 'A');
+	myScreen.set('#').display(cout);
+	cout << endl;
+	blank.display(cout);
+	*/
+	// 7.27
+	Screen myScreen(5, 5, 'X');
+	myScreen.move(4, 0).set('#').display(cout);
+	cout << "\n";
+	myScreen.display(cout);
+	cout << "\n";
+    
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
