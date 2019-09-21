f1b3a367d58bdc63de5355fa718119238cb33e8c
xixuecao <xixuecao@gmail.com>
2019-09-22 07:49:53 +0800

    section 12.1.1

---
+++ C++/12_1_1/12_1_1/12_1_1.cpp
@@ -0,0 +1,66 @@
+﻿// 12_1_1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <memory>
+#include <vector>
+
+using namespace std;
+
+class StrBlob {
+public:
+	typedef vector<string>::size_type size_type;
+	StrBlob();
+	StrBlob(initializer_list<string> li);
+
+	size_type size() const { return data->size(); }
+	bool empty() const { return data->empty(); }
+
+	void push_back(const string& t) { data->push_back(t); }
+	void pop_back();
+	string& front();
+	string& back();
+
+private:
+	shared_ptr<vector<string>> data;
+	void check(size_type i, const string& msg) const;
+};
+StrBlob::StrBlob() :data(make_shared<vector<string>>()) {}
+StrBlob::StrBlob(initializer_list<string> li) : data(make_shared<vector<string>>(li)) {}
+
+void StrBlob::check(size_type i, const  string& msg) const{
+	if (i >= data->size())
+	{
+		throw out_of_range(msg);
+	}
+}
+
+void StrBlob::pop_back() {
+	check(0, "pop_back on empty StrBlob");
+	data->pop_back();
+}
+
+string& StrBlob::front() {
+	check(0, "front on empty StrBlob");
+	return data->front();
+}
+
+string& StrBlob::back() {
+	check(0, "back on empty StrBlob");
+	return data->back();
+}
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
